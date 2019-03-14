96fb44c8d1e124bf7fd87d53da35534fb80fcccf
Mark-Yoo <yjinhyuk@gmail.com>
2019-03-14 18:24:27 +0900

    feat. javascript version todolist

---
+++ JS method basic/jquery.example.js
@@ -0,0 +1,14 @@
+/*
+    웹 브라우저가 문서를 표시하는 순서
+    1. HTML 로드
+    2. DOM 생성
+    3. $(document).ready() 실행
+    4. 이미지 등 요소 로드
+    5. 페이지 로딩 완료
+    6. $(window).ready() // load는 3.0에서 removed, 1.8 deprecated
+    
+    Event
+    Form : blur(포커스를 잃었을 때), change
+    Mouse : cclick, contextmenu, dblclick, mousedown, mouseenter(별로 안 씀), mouseleave, mousemove, mouseout, mouseover, mouseup
+    keboard : keydown, keypress, keyup
+*/
